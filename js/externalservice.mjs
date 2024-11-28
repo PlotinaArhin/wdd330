bf5f8d6e0e89bc8ca1542caa9d78239ebf0a4341
PlotinaArhin <arhinplotina@gmail.com>
2024-11-28 14:14:22 +0000

    made changes

---
+++ js/externalservice.mjs
@@ -0,0 +1,16 @@
+export default class ExternalServices {
+    static async checkout(payload) {
+      const url = "https://wdd330-backend.onrender.com/checkout";
+      const options = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      };
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    }
+  }
+  
